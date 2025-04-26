c4efb17f2f13711068a13969e87122fd56d86516
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2025-04-26 19:46:52 +0200

    feat: policy risk groups

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
     } as AliasOptions,
   },
   preview: {
-    port: 3000,
+    port: 3001,
    },
    server: {
-    port: 3000,
+    port: 3001,
     host: true,
     watch: {
       usePolling: true,
